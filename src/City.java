71bbc870fc10493ad24785f308a89375022449dc
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-08-17 11:50:56 +0200

    Exercices jusqu'à 1.7 inclus

--- src/City.java
+++ src/City.java
@@ -1,15 +1,88 @@
 
 
 public class City {
-	public String nameCity;
-	public String nameCountry;
+	private String nameCity;
+	private String nameCountry;
 	//public Country country;
-	public int nbInHabitants;
+	private int nbInHabitants;
+	public static int nbCitys;
 	
 	public City(String nameCity, String nameCountry, int nbInHabitants) {
 		this.nameCity = nameCity;
 		this.nameCountry = nameCountry;
 		this.nbInHabitants = nbInHabitants;
+		this.nbCitys++;
+	}
+	
+	public City(String nameCity, int nbInHabitants) {
+		this.nameCity = nameCity;
+		this.nameCountry = "";
+		this.nbInHabitants = nbInHabitants;
+		this.nbCitys++;
+	}
+	
+	public void display() {
+		String strNameCountry = this.nameCountry != "" ? nameCountry : "unknown";
+		System.out.println("Ville de " + this.nameCity + " en " + strNameCountry + " ayant " + this.nbInHabitants + " habitants");
+	}
+	
+	public void beautifulDisplay() {
+		String strNameCountry = this.nameCountry != "" ? nameCountry : "unknown";
+		System.out.println("[Ville : " + this.nameCity + "] [Pays : " + strNameCountry + "] [Nombre d'habitants : " + this.nbInHabitants + "]");
+	}
+
+	
+	public String toString() {
+		String strNameCountry = this.nameCountry != "" ? nameCountry : "unknown";
+		return "Ville de " + this.nameCity + " en " + strNameCountry + " ayant " + this.nbInHabitants + " habitants";
+	}
+
+	public String getNameCity() {
+		return nameCity;
+	}
+
+	public void setNameCity(String nameCity) {
+		this.nameCity = nameCity;
+	}
+
+	public String getNameCountry() {
+		 return nameCountry;
+	}
+
+	public void setNameCountry(String nameCountry) {
+		this.nameCountry = nameCountry;
+	}
+
+	public int getNbInHabitants() {
+		return nbInHabitants;
+	}
+
+	public void setNbInHabitants(int nbInHabitants) {
+		if (nbInHabitants > 0) {
+			this.nbInHabitants = nbInHabitants;
+		}else {
+			System.out.println("ERREUR - Le nombre d'habitants ne peut pas être inférieur à 0 !");
+		}
+	}
+	
+	public int getNbCitys() {
+		return nbCitys;
+	}
+
+	public void setNbCitys(int nbCitys) {
+		this.nbCitys = nbCitys;
+	}
+
+	public void reductPopulation(int nbReduct) {
+		if (nbReduct < this.nbInHabitants) {
+			this.nbInHabitants = this.nbInHabitants - nbReduct;
+		}else {
+			System.out.println("ERREUR - Le nombre d'habitants à enlever ne peut pas être inférieur au nombre d'habitants !");
+		}
+	}
+	
+	public void addPopulation(int nbReduct) {
+		this.nbInHabitants = this.nbInHabitants + nbReduct;
 	}
 	
 }
--- src/TestCity.java
+++ src/TestCity.java
@@ -4,19 +4,54 @@ public class TestCity {
 	
 	public static void main(String[] args){
 		
-		//Exercice 1.1
+		//Exercice 1.1 
+		System.out.println("Exercice 1.1");
 		City toulouse = new City("Toulouse", "France", 450000);
 		City merignac = new City("Mérignac", "France", 78000);
 		City bayonne = new City("Bayonne", "France", 56000);
 		City stgeours = new City("Saint-Geours-de-Maremne", "France", 2900);
-		System.out.println("Ville : " + toulouse.nameCity + " (" + toulouse.nameCountry + ") - Nombre d'habitants : " + toulouse.nbInHabitants);
-		System.out.println("Ville : " + merignac.nameCity + " (" + merignac.nameCountry + ") - Nombre d'habitants : " + merignac.nbInHabitants);
-		System.out.println("Ville : " + bayonne.nameCity + " (" + bayonne.nameCountry + ") - Nombre d'habitants : " + bayonne.nbInHabitants);
-		System.out.println("Ville : " + stgeours.nameCity + " (" + stgeours.nameCountry + ") - Nombre d'habitants : " + stgeours.nbInHabitants);
-		toulouse.nbInHabitants = toulouse.nbInHabitants + 20000;
-		System.out.println("Après mise à jour, Ville : " + toulouse.nameCity + " (" + toulouse.nameCountry + ") - Nombre d'habitants : " + toulouse.nbInHabitants);
+		
+		toulouse.beautifulDisplay();
+		merignac.beautifulDisplay();
+		bayonne.beautifulDisplay();
+		stgeours.beautifulDisplay();
+		//toulouse.nbInHabitants = toulouse.nbInHabitants + 20000;		
 		
 		//Exercice 1.2
+		System.out.println("\n Exercice 1.2");
+		//toulouse.nbInHabitants = -200;
+		toulouse.setNbInHabitants(-200);
+		toulouse.beautifulDisplay();
+		toulouse.reductPopulation(500000);
+		toulouse.beautifulDisplay();
+		toulouse.reductPopulation(100000);
+		toulouse.beautifulDisplay();
+		
+		//Exercice 1.3
+		System.out.println("\n Exercice 1.3");
+		City rabat = new City("Rabat", 577000);
+		rabat.beautifulDisplay();
+		rabat.setNameCountry("Maroc");
+		rabat.beautifulDisplay();
+		
+		//Exercice 1.4 et 1.5 : En fait j'avais déjà écrit la méthode toString! Et effectivement si je crée pas toString le System.out.println(toulouse); va planter
+		//J'ai quand même créé la .display... 
+		System.out.println("\n Exercice 1.4");
+		toulouse.display();
+		
+		//Exercice 1.6 
+		//J'avais déjà mis un toString, j'ai mis mon toString dans une nouvelle méthode beautifulDisplay... pour pas casser mes exos précédents!
+		System.out.println("\n Exercice 1.6");
+		System.out.println(toulouse);
+		
+		//Exercice 1.7
+		System.out.println("Nombre de villes créées : " + City.nbCitys);
+		City paris = new City("Paris", "France", 2000000);
+		City nice = new City("Nice", "France", 340000);
+		System.out.println("Nombre de villes créées : " + City.nbCitys);
+		
+		//Exercice 1.8
+		
 		
 		
 	}
